0a0d31b883a1ddb891600681c5835b73f1c3f451
svrodela <svrodela@gmail.com>
2025-01-21 14:27:48 -0600

    products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Products(models.Model):
+    name=models.TextField(max_length=100,verbose_name="producto")
+    description=models.TextField(max_length=300,verbose_name="descripción")
+    price=models.DecimalField(max_digits=10,decimal_places=2,verbose_name="precio")
+    available=models.BooleanField(default=True,verbose_name="disponible")
+    picture=models.ImageField(upload_to="pictures",null=True,blank=True,verbose_name="imagen")
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from products.models import Product
 
 # Create your views here.
+
+class ListProductsView(TemplateView):
+    template_name="products/list_products.html"
+
+    def get_context_data(self, **kwargs):
+        products=Product.objects.all()
+        context=super().get_context_data(**kwargs)
+        context['list_products']=products
+        return context
+        
